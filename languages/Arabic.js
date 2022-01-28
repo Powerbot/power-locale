e58222862a21140eddbadfc77eb775090ca6548e
Mohammad <74769232+zmkq@users.noreply.github.com>
2022-01-28 18:04:48 +0200

    New translations english.js (Arabic)

--- languages/Arabic.js
+++ languages/Arabic.js
@@ -439,6 +439,8 @@ module.exports = {
     images_as_only: 'على انه شات خاص بالصور فقط! | لإعادة ضبط الشات قم بكتابة: imageschannel reset <channel>',
     slowmode_specify_time: '**:rolling_eyes: - رجاءً قم بتحديد وقت صالح!**',
     slowmode_done: '<:check:855651069317480488> ** الوضع البطيء الآن',
+    drop_support: `Hello, {author}\n<:information:936646340455309362> In this update, we dropped support of normal commands.\n
+  **<:SlashCommands:930520506841567333> Please refer to use Slash Commands (/)**`,
     // Permissions
     manage_emojis: '<:x_:873534429346267157> آسف، ليس لديك إذن [MANAGE_EMOJIS]',
     manage_webhooks: '<:x_:873534429346267157> آسف، ليس لديك إذن [MANAGE_WEBHOOKS]',
